54eda6b869e44f48c03a8d5efe75778e87f5c51f
ChadoNihi <chadoishere@gmail.com>
2017-12-22 21:58:07 +0200

    init new mix project

---
+++ lib/sessionizator.ex
@@ -0,0 +1,18 @@
+defmodule Sessionizator do
+  @moduledoc """
+  Documentation for Sessionizator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sessionizator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sessionizator_test.exs
@@ -0,0 +1,8 @@
+defmodule SessionizatorTest do
+  use ExUnit.Case
+  doctest Sessionizator
+
+  test "greets the world" do
+    assert Sessionizator.hello() == :world
+  end
+end
